2b60ce74d1b9c3092e26c9e6d18f1e559709e952
mxxt1 <matt@dustcloudlabs.com>
2020-02-04 14:32:18 -0500

    testing cors as middleware directly on the fetchUserSchema route

--- routes/users/userRoles.js
+++ routes/users/userRoles.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const { fetchUserSchema, assignUserRole } = require('./utils');
+const cors = require('cors');
 
 // * RETRIEVE THE FULL USER SCHEMA FROM AUTH0
-router.post('/', async (req, res) => fetchUserSchema(req, res))
+router.post('/', cors(), async (req, res) => fetchUserSchema(req, res))
 
 // * USER INFORMATION TO BE UPDATED FOR ROLE ASSIGNMENT.
-router.put('/:id', async (req, res) => assignUserRole(req, res))
+router.put('/:id', cors(), async (req, res) => assignUserRole(req, res))
 
 module.exports = router;
