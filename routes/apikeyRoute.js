ba32b04b81772f4fdb01ada597077e5376739472
BaoPham92 <baopham92@yahoo.com>
2020-02-05 17:56:11 -0500

    Authorized request call to the Management API adjustment.

--- routes/apikeyRoute.js
+++ routes/apikeyRoute.js
@@ -9,12 +9,14 @@ const jwtCheck = require('../middleware/token-middleware')
 const rules = require('../middleware/rules/rules-middleware');
 
 router.post('/private', jwtCheck, rules, async (req, res) => {
+  // * REQUEST PROMISE SETUP FOR AUTHORIZED API CALLS WITH TOKEN BY THE API MANAGEMENT.
+  const request = require('request-promise');
+  const options = {
+    method: 'GET',
+    url: `https://sauti-africa-market-prices.auth0.com/api/v2/users/${req.body.id}`,
+    headers: { authorization: `Bearer ${process.env.api_token}` }
+  };
   const key = uuidAPIKey.create()
-  const { id } = req.body;
-  let role;
-
-  req.body.sub = id;
-
   //generate new date to be written to table
   const date = new Date();
   //get the exact date in milliseconds
@@ -29,43 +31,45 @@ router.post('/private', jwtCheck, rules, async (req, res) => {
     "sub": `${req.body.sub}`
   }
 
-  //retrieves the expanded user object from auth0 which contains app_metadata.role
-
-  axios.post('https://sauti-marketprice-data.herokuapp.com/api/users', idObject)
-  .then(response => {
-   const role = response.data.app_metadata.role;
-    return role
+  request(options, async function (error, response, body) {
+    if (error) console.log(error);
+    return JSON.parse(response)
   })
-  .then(role => {
-    bcrypt.hash(key.apiKey, 10, async (_err, hash) => {
-      if (user) {
-        try {
-          await db('apiKeys')
-            .where({ user_id: id })
-            //update table with key hash. Don't reset reset_date.
-            .update({ key: hash, user_role: role})
-          res.status(200).json({ existed: true, key: key.apiKey })
-        } catch (err) {
-          console.log(err)
-        }
-      } else {
-        try {
-          await db('apiKeys').insert({ 
-            key: hash, 
-            user_id: id, 
-            reset_date:dateMilliseconds,
-            user_role: role
-          })
-          res.status(200).json({ existed: false, key: key.apiKey })
-        } catch (err) {
-          res.status(500).json({Message: `There was an error when updating the API key`, Error: err})
-        }
-      }
+    .then(async res => {
+      const result = await JSON.parse(res);
+      return result.app_metadata.role
     })
-  })
-  .catch(err => {
-    console.log(err)
-  });
+    .catch(err => console.log(err))
+    .then(role => {
+      bcrypt.hash(key.apiKey, 10, async (_err, hash) => {
+        if (user) {
+          try {
+            await db('apiKeys')
+              .where({ user_id: id })
+              //update table with key hash. Don't reset reset_date.
+              .update({ key: hash, user_role: role })
+            res.status(200).json({ existed: true, key: key.apiKey })
+          } catch (err) {
+            // console.log(err)
+          }
+        } else {
+          try {
+            await db('apiKeys').insert({
+              key: hash,
+              user_id: id,
+              reset_date: dateMilliseconds,
+              user_role: role
+            })
+            res.status(200).json({ existed: false, key: key.apiKey })
+          } catch (err) {
+            res.status(500).json({ Message: `There was an error when updating the API key`, Error: err })
+          }
+        }
+      })
+    })
+    .catch(err => {
+      // console.log(err)
+    });
 })
 
 module.exports = router
