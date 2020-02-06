bbd025dded8c0e77891aad7e8324d50e5cdaaa37
BaoPham92 <baopham92@yahoo.com>
2020-02-06 14:36:03 -0500

    try catch removed

--- routes/apikeyRoute.js
+++ routes/apikeyRoute.js
@@ -44,34 +44,26 @@ router.post('/private', jwtCheck, rules, async (req, res) => {
     .then(role => {
       bcrypt.hash(key.apiKey, 10, async (_err, hash) => {
         if (user) {
-          try {
-            await db('apiKeys')
-              .where({ user_id: req.body.id })
-              //update table with key hash. Don't reset reset_date.
-              .update({ key: hash, user_role: role })
-              res.header("Access-Control-Allow-Origin", "https://jolly-panini-1f3c1c.netlify.com/profile");
-              res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            res.status(200).json({ existed: true, key: key.apiKey })
-          } catch (err) {
-            // console.log(err)
-          }
+          await db('apiKeys')
+            .where({ user_id: req.body.id })
+            //update table with key hash. Don't reset reset_date.
+            .update({ key: hash, user_role: role })
+          res.header("Access-Control-Allow-Origin", "https://jolly-panini-1f3c1c.netlify.com/profile");
+          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+          res.status(200).json({ existed: true, key: key.apiKey })
         } else {
-          try {
-            await db('apiKeys').insert({
-              key: hash,
-              user_id: req.body.id,
-              reset_date: dateMilliseconds,
-              user_role: role
-            })
-            res.status(200).json({ existed: false, key: key.apiKey })
-          } catch (err) {
-            res.status(500).json({ Message: `There was an error when updating the API key`, Error: err })
-          }
+          await db('apiKeys').insert({
+            key: hash,
+            user_id: req.body.id,
+            reset_date: dateMilliseconds,
+            user_role: role
+          })
+          res.status(500).json({ existed: false, key: key.apiKey })
         }
       })
     })
     .catch(err => {
-      // console.log(err)
+      console.log(err)
     });
 })
 
