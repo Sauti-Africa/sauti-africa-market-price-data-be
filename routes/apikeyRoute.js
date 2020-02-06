245facf93fbabacc04b0035795e0b370414ad5a4
BaoPham92 <baopham92@yahoo.com>
2020-02-06 14:46:37 -0500

    adjusted for new api settings on auth0

--- routes/apikeyRoute.js
+++ routes/apikeyRoute.js
@@ -44,26 +44,32 @@ router.post('/private', jwtCheck, rules, async (req, res) => {
     .then(role => {
       bcrypt.hash(key.apiKey, 10, async (_err, hash) => {
         if (user) {
-          await db('apiKeys')
-            .where({ user_id: req.body.id })
-            //update table with key hash. Don't reset reset_date.
-            .update({ key: hash, user_role: role })
-          res.header("Access-Control-Allow-Origin", "https://jolly-panini-1f3c1c.netlify.com/profile");
-          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-          res.status(200).json({ existed: true, key: key.apiKey })
+          try {
+            await db('apiKeys')
+              .where({ user_id: req.body.id })
+              //update table with key hash. Don't reset reset_date.
+              .update({ key: hash, user_role: role })
+            res.status(200).json({ existed: true, key: key.apiKey })
+          } catch (err) {
+            // console.log(err)
+          }
         } else {
-          await db('apiKeys').insert({
-            key: hash,
-            user_id: req.body.id,
-            reset_date: dateMilliseconds,
-            user_role: role
-          })
-          res.status(500).json({ existed: false, key: key.apiKey })
+          try {
+            await db('apiKeys').insert({
+              key: hash,
+              user_id: req.body.id,
+              reset_date: dateMilliseconds,
+              user_role: role
+            })
+            res.status(200).json({ existed: false, key: key.apiKey })
+          } catch (err) {
+            res.status(500).json({ Message: `There was an error when updating the API key`, Error: err })
+          }
         }
       })
     })
     .catch(err => {
-      console.log(err)
+      // console.log(err)
     });
 })
 
