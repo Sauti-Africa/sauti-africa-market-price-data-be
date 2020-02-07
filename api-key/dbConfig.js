6249071863196af05e42723b9f39548536dd8484
mxxt1 <matt@dustcloudlabs.com>
2020-02-07 14:03:22 -0500

    removed old migration, modified package json with current repo URL

--- api-key/dbConfig.js
+++ api-key/dbConfig.js
@@ -3,6 +3,8 @@ const secrets = require('../config/secrets.js')
 
 const environment = secrets.environment
 
+console.log(`environment `,environment)
+
 const config = require('../knexfile.js')[environment]
 // we must select the development object from our knexfile
 // const db = knex(config.environment) //TODO: change this to be dynamic later
